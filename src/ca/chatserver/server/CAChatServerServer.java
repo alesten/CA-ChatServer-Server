6dc4eb58605320ec6ffd31cd76244f115ba32b95
Alexander Steen <AlexanderSteen@Alexanders-MacBook-Pro.local>
2015-09-08 09:45:00 +0200

    Added server properties

--- src/ca/chatserver/server/CAChatServerServer.java
+++ src/ca/chatserver/server/CAChatServerServer.java
@@ -5,17 +5,34 @@
  */
 package ca.chatserver.server;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.util.Properties;
+import utils.Utils;
+
 /**
  *
  * @author AlexanderSteen
  */
 public class CAChatServerServer {
 
+    private static final Properties properties = Utils.initProperties("Server.properties");
+
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        String ip = properties.getProperty("serverIp");
+        int port = Integer.parseInt(properties.getProperty("port"));
+        if(args.length == 2){
+            ip = args[0];
+            port = Integer.parseInt(args[1]);
+        }
+        
+        ServerSocket ss = new ServerSocket();
+        ss.bind(new InetSocketAddress(ip, port));
     }
     
 }
