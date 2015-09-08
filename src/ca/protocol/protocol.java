ab8806c62a1d83177a54cb79619c1f2f60d0f3d5
Rolf88 <cb88@jubii.dk>
2015-09-08 10:50:14 +0200

    protocol

---
+++ src/ca/protocol/protocol.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.protocol;
+
+/**
+ *
+ * @author RolfMoikjær
+ */
+public class protocol {
+
+    public static final String USER = "USER#";
+    public static final String MSG = "MSG#";
+    public static final String STOP = "STOP#";
+    public static final String USERLIST = "USERLIST#";
+
+}
